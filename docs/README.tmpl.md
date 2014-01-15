a8eb6edbe1e83809c775a718fd1b15095e4fb98f
Brian Woodward <brian.woodward@gmail.com>
2014-01-14 19:20:50 -0500

    initial setup

---
+++ docs/README.tmpl.md
@@ -0,0 +1,21 @@
+# {%= name %} {%= _.badge("fury") %} {%= _.badge("travis") %}
+
+> {%= description %}
+
+## Getting Started
+{%= _.doc("quickstart.md") %}
+
+## Contributing
+{%= _.contrib("contributing.md") %}
+
+## Author
+{%= _.contrib("authors.md") %}
+
+
+## License
+{%= copyright %}
+{%= license %}
+
+***
+
+{%= _.include("footer.md") %}
---
+++ test/main.js
@@ -0,0 +1,22 @@
+/**
+ * Pluggable
+ * Copyright (c) 2014 Jon Schlinkert, Brian Woodward, contributors
+ * Licensed under the MIT License (MIT).
+ */
+
+var expect = require('chai').expect;
+var _ = require('lodash');
+
+describe('pluggable', function() {
+
+	before(function(){
+		// run any code before tests here
+	});
+
+  it('should do something awesome', function() {
+    var expected = 'FOO';
+    var actual = 'FOO';
+    expect(actual).to.eql(expected);
+  });
+
+});
