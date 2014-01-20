13344275980063653a286919b0dbbe050b606b95
Brian Woodward <brian.woodward@gmail.com>
2014-01-20 12:50:49 -0500

    fixing parameters being passed to plugins

--- test/main.js
+++ test/main.js
@@ -6,19 +6,40 @@
 
 var expect = require('chai').expect;
 var _ = require('lodash');
+var events = require('events');
 
 var Dovetail = require('../');
 
+// test app - inherit from EventEmiteter
+var App = function() {
+  events.EventEmitter.call(this);
+  this.appName = 'test-app';
+};
+
+require('util').inherits(App, events.EventEmitter);
+
 describe('Dovetail', function() {
 
-	before(function(){
-		// run any code before tests here
-	});
+  before(function(){
+    // run any code before tests here
+  });
 
-  it('should do something awesome', function() {
-    var expected = 'FOO';
-    var actual = 'FOO';
-    expect(actual).to.eql(expected);
+  it('should create and run a plugin', function(done) {
+
+    // add a test stage
+    Dovetail.plugins.addStage('first', 'first');
+
+    var app = new App();
+    var dovetail = new Dovetail(app);
+
+    // add a new plugin
+    dovetail.createPlugin('test-plugin', 'this is just a test', { stage: 'first' }, function (params, next) {
+      console.log('params', params);
+      next();
+    });
+
+    // run the plugin
+    dovetail.runStage('first', { foo: 'bar' }, done);
   });
 
 });
