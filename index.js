da3e1bac40c566af81525852dbb046749a34f13e
jonschlinkert <dev@sellside.com>
2014-05-10 02:56:58 -0400

    update deps, replace boson with plasma, other minor changes. @doowb, you'll need to look at the logic for plasma. there is no documentation and there aren't any unit tests to explain what the expected result is, so I just wired it up to return what boson was returning, given the limited information at hand.

--- index.js
+++ index.js
@@ -13,13 +13,13 @@ var path = require('path');
 
 // node_modules
 var glob = require('globule');
-var boson = require('boson');
+var plasma = require('plasma');
 var async = require('async');
 var _ = require('lodash');
 
 // local modules
-var events = require('./events');
-var Middleware = require('./middleware');
+var events = require('./lib/events');
+var Middleware = require('./lib/middleware');
 
 // return true if a direct match or a wildcard match
 var isEventMatch = function (a, b) {
@@ -27,48 +27,57 @@ var isEventMatch = function (a, b) {
 };
 
 
-var Dovetail = module.exports = function (app, logger) {
+function Dovetail(app, logger) {
+  if (!(this instanceof Dovetail)) {
+    return new Dovetail();
+  }
+
   this.eventCache = {};
   this.cache = {};
-
   this.app = app;
   this.logger = logger;
 
   // make sure each instance has access to events in case something else needs them
   this.events = events;
 };
-
 Dovetail.events = events;
+
 
 Dovetail.prototype.setEventCache = function (event, middleware) {
   (this.eventCache[event] = this.eventCache[event] || []).push(middleware);
 };
 
+
 Dovetail.prototype.getEventCache = function (event) {
   return (this.eventCache[event] || []);
 };
+
 
 Dovetail.prototype.setCache = function (name, middleware) {
   this.cache[name] = middleware;
 };
 
+
 Dovetail.prototype.getCache = function (name) {
   return this.cache[name];
 };
 
-Dovetail.prototype.expandEvents = function (middlewareEvents) {
-  if (!_.isArray(middlewareEvents)) {
-    middlewareEvents = [middlewareEvents];
-  }
+
+Dovetail.prototype.parseEvents = function (_events) {
+  _events = !Array.isArray(_events) ? [_events] : _events;
 
   return _.filter(events.all(), function (event) {
-    var eventParts = event.split(':');
+    var segment = event.split(':');
     var isMatch = false;
-    _.map(middlewareEvents, function (middlewareEvent) {
-      var middlewareParts = middlewareEvent.split(':');
-      isMatch = isMatch || (isEventMatch(middlewareParts[0], eventParts[0]) &&
-        isEventMatch(middlewareParts[1], eventParts[1]) &&
-        isEventMatch(middlewareParts[2], eventParts[2]));
+
+    _.map(_events, function (middlewareEvent) {
+      var stage = middlewareEvent.split(':');
+
+      var first  = isEventMatch(stage[0], segment[0]);
+      var second = isEventMatch(stage[1], segment[1]);
+      var third  = isEventMatch(stage[2], segment[2]);
+
+      isMatch = isMatch || (first && second && third);
     });
     return isMatch;
   });
@@ -76,21 +85,23 @@ Dovetail.prototype.expandEvents = function (middlewareEvents) {
 
 Dovetail.prototype.addListeners = function (events, middleware) {
   var self = this;
+
   // setup a listener for each event this middleware registered
-  events = self.expandEvents(events);
-  events.forEach(function (event) {
-    self.setEventCache(event, middleware);
-    //middleware.listen(event);
+  var _events = self.parseEvents(events);
+
+  _events.forEach(function (_event) {
+    self.setEventCache(_event, middleware);
+    //middleware.listen(_event);
   });
 };
 
 
 Dovetail.prototype.use = function (fn, options) {
-
+  // Clone the options to avoid mutating upstream variables
   options = _.extend({}, options);
 
   // name must be provided
-  if (!options.name || !_.isString(options.name)) {
+  if (!options.name) {
     throw new Error('Must supply the name parameter.');
   }
 
@@ -116,6 +127,7 @@ Dovetail.prototype.use = function (fn, options) {
   return middleware;
 };
 
+
 /**
  * Find middleware and load them based on the list of middleware
  * @param  {[type]} app     [description]
@@ -123,15 +135,19 @@ Dovetail.prototype.use = function (fn, options) {
  * @param  {[type]} options  [description]
  * @return {[type]}          [description]
  */
+
 Dovetail.prototype.resolve = function (_middleware) {
   var self = this;
-  var actualMiddleware = boson(_middleware, self.app);
+  var actualMiddleware = plasma(_middleware, {config: self.app});
+
   // set middleware options
   return _.map(_.keys(actualMiddleware), function (key) {
     var middleware = actualMiddleware[key];
     middleware.options = middleware.options || {};
+
     // options could be directly on the middleware object
     var options = middleware.options;
+
     options.event = options.event || (options.event = middleware.options.events || middleware.event || middleware.events || 'default');
     options.name = options.name || middleware.name || key;
     options.app = options.app || middleware.app || self.app;
@@ -143,32 +159,36 @@ Dovetail.prototype.resolve = function (_middleware) {
 Dovetail.prototype.run = function (params, done) {
   var self = this;
   var allEvents = events.all();
+
   async.forEachSeries(_.keys(allEvents), function (key, next) {
-      var event = allEvents[key];
-      self.runevent(event, params, next);
-    },
-    function (err) {
-      if (err) {
-        self.logger.error(err);
-        done();
-      } else {
-        done();
-      }
-    });
+    var event = allEvents[key];
+    self.runevent(event, params, next);
+
+  }, function (err) {
+    if (err) {
+      self.logger.error(err);
+      done();
+    } else {
+      done();
+    }
+  });
 };
 
 Dovetail.prototype.runEvent = function (event, params, done) {
   var self = this;
   var eventCache = self.getEventCache(event);
+
   if (eventCache && eventCache.length) {
     params = _.extend({event: event}, params);
-    async.eachSeries(eventCache,
-      function (middleware, next) {
-        middleware.run(params, next);
-      },
-    done);
+
+    async.eachSeries(eventCache, function (middleware, next) {
+      middleware.run(params, next);
+    }, done);
 
   } else {
     done();
   }
 };
+
+
+module.exports = Dovetail;
